b66a69a99c67924e0502a5f9bbc8f39cb3869e02
Peerasak Unsakon <clonezer@gmail.com>
2020-04-27 08:55:59 +0700

    audit fix vulnerable

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,16 @@
 <template>
   <v-app>
     <v-content>
-      <v-row
-        class="ma-5"
-        justify="center"
-      >
-        <v-col
-          cols="12"
-          md="8"
-          lg="6"
-        >
+      <v-row class="ma-5" justify="center">
+        <v-col cols="12" md="8" lg="6">
           <v-card :elevation="24">
             <v-row class="pa-6">
-              <v-col
-                cols="12"
-                md="5"
-                lg="4"
-              >
-                <v-img
-                  src="@/assets/mangosteen.jpg"
-                  position="center center"
-                  height="100%"
-                >
-                </v-img>
+              <v-col cols="12" md="5" lg="4">
+                <v-img src="@/assets/mangosteen.jpg" position="center center" height="100%"></v-img>
               </v-col>
-              <v-col
-                cols="12"
-                md="7"
-                lg="8"
-              >
-                <div class="title">
-                  มังคุดออร์แกนิก ออนไลน์
-                </div>
-                <v-form
-                  ref="form"
-                  action=""
-                  v-model="valid"
-                  lazy-validation
-                >
+              <v-col cols="12" md="7" lg="8">
+                <div class="title">มังคุดออร์แกนิก ออนไลน์</div>
+                <v-form ref="form" action v-model="valid" lazy-validation>
                   <v-text-field
                     v-model="name"
                     :counter="280"
@@ -62,26 +35,13 @@
                     required
                   ></v-select>
 
-                  <v-row
-                    class="px-5"
-                    justify="space-between"
-                  >
-                    <v-btn
-                      color="error"
-                      @click="reset"
-                      :loading="loading"
-                    >
-                      <v-icon left>mdi-delete</v-icon>
-                      ล้างข้อมูล
+                  <v-row class="px-5" justify="space-between">
+                    <v-btn color="error" @click="reset" :loading="loading">
+                      <v-icon left>mdi-delete</v-icon>ล้างข้อมูล
                     </v-btn>
 
-                    <v-btn
-                      color="success"
-                      @click="submit"
-                      :loading="loading"
-                    >
-                      <v-icon left>mdi-shopping</v-icon>
-                      สั่งจองเลย
+                    <v-btn color="success" @click="submit" :loading="loading">
+                      <v-icon left>mdi-shopping</v-icon>สั่งจองเลย
                     </v-btn>
                   </v-row>
                 </v-form>
@@ -91,13 +51,7 @@
         </v-col>
         <v-snackbar v-model="snackbar">
           {{ alertMessage }}
-          <v-btn
-            color="pink"
-            text
-            @click="snackbar = false"
-          >
-            ปิด
-          </v-btn>
+          <v-btn color="pink" text @click="snackbar = false">ปิด</v-btn>
         </v-snackbar>
       </v-row>
     </v-content>
@@ -105,27 +59,27 @@
 </template>
 
 <script>
-import qs from 'querystring';
+import qs from "querystring";
 
 export default {
-  name: 'App',
+  name: "App",
   data: () => ({
     loading: false,
     valid: true,
-    name: '',
+    name: "",
     snackbar: false,
-    alertMessage: '',
+    alertMessage: "",
     nameRules: [
-      v => !!v || 'กรุณากรอกชื่อผู้จอง',
-      v => (v && v.length <= 280) || 'ชื่อไม่ควรเกิน 280 ตัวอักษร',
+      v => !!v || "กรุณากรอกชื่อผู้จอง",
+      v => (v && v.length <= 280) || "ชื่อไม่ควรเกิน 280 ตัวอักษร"
     ],
-    phone: '',
+    phone: "",
     phoneRules: [
-      v => !!v || 'กรุณาระบุเบอร์โทรศัพท์ของท่าน',
-      v => (/\d{9,10}/.test(v) && v.length <= 10) || 'เบอร์โทรศัพท์ไม่ถูกต้อง',
+      v => !!v || "กรุณาระบุเบอร์โทรศัพท์ของท่าน",
+      v => (/\d{9,10}/.test(v) && v.length <= 10) || "เบอร์โทรศัพท์ไม่ถูกต้อง"
     ],
     selectedPackage: null,
-    packages: ['5 กก.', '10 กก.', '15 กก.', '20 กก.'],
+    packages: ["5 กก.", "10 กก.", "15 กก.", "20 กก."]
   }),
   methods: {
     reset() {
@@ -133,18 +87,19 @@ export default {
     },
     async submit() {
       if (this.$refs.form.validate()) {
-        const url =          'https://docs.google.com/forms/d/e/1FAIpQLSeRSWxqaW7BvoF47cl_muvGTvw39sm7Qt6DNYL0CkxQNVR0Ww/formResponse';
+        const url =
+          "https://docs.google.com/forms/d/e/1FAIpQLSeRSWxqaW7BvoF47cl_muvGTvw39sm7Qt6DNYL0CkxQNVR0Ww/formResponse";
         const body = {
-          'entry.1819278497': this.name,
-          'entry.1633641327': this.phone,
-          'entry.828954501': this.selectedPackage,
+          "entry.1819278497": this.name,
+          "entry.1633641327": this.phone,
+          "entry.828954501": this.selectedPackage
         };
 
         try {
           this.loading = true;
           await this.postData(url, body);
           this.reset();
-          this.alertMessage = 'สั่งจองให้เรียบร้อยแล้ว';
+          this.alertMessage = "สั่งจองให้เรียบร้อยแล้ว";
         } catch (error) {
           this.alertMessage = error.message;
         } finally {
@@ -155,14 +110,14 @@ export default {
     },
     postData(url, data) {
       return fetch(url, {
-        method: 'POST',
-        mode: 'no-cors',
+        method: "POST",
+        mode: "no-cors",
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
+          "Content-Type": "application/x-www-form-urlencoded"
         },
-        body: qs.stringify(data),
+        body: qs.stringify(data)
       });
-    },
-  },
+    }
+  }
 };
 </script>
